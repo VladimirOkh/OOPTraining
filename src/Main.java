97ee286e2b5fc19390be6907ea6077930abd5fff
Vladimir Okhremenko <ohremenkovwork@gmail.com>
2023-05-01 15:42:35 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Zombie zombie1 = new Zombie("Bob");
+        zombie1.attack();
+
+//        Zombie zombie2 = new Zombie("Aaron", 10);
+//        zombie2.attack();
+    }
+}
---
+++ src/Monster.java
@@ -0,0 +1,46 @@
+public class Monster {
+    private int HP;
+    private int damage;
+    private String name;
+
+    public Monster(String name, int damage){
+        System.out.println("Monster " + name + " was created");
+        this.name = name;
+        this.damage = damage;
+    }
+
+    public void growl(){
+        System.out.printf(" " + name + " growled");
+    }
+
+    public void attack(){
+        System.out.println("Monster " + name +  " attacked with damage " + damage);
+    }
+
+
+
+
+    public int getHP() {
+        return HP;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
