056f3ae6c7070bbab5595ff51fe637b253baa1c5
Vladimir Okhremenko <ohremenkovwork@gmail.com>
2023-05-01 16:32:31 +0300

    add new Monster - GiantSnake

---
+++ src/GiantSnake.java
@@ -0,0 +1,29 @@
+public class GiantSnake extends Monster{
+    public static String scream = "Ssssss";
+
+    public GiantSnake(String name) {
+        super(name + " the Snake", 5);
+
+    }
+
+    @Override
+    public void attack() {
+        super.attack();
+        growl();
+        System.out.println("     ...and hid in the grass");
+    }
+
+    @Override
+    public void growl() {
+        System.out.print(scream);
+        super.growl();
+    }
+
+    public void growl(boolean isLoud) {
+        if(isLoud) {
+            System.out.print(scream.toUpperCase());
+            super.growl();
+        }
+        else growl();
+    }
+}
